872225f0d9936be7fabd2b76dda6bc2ce2e12ff6
Peter Peterson <peterfpeterson@gmail.com>
2010-09-22 13:45:16 -0400

    Checkpointing the working code before tearing through with externalizing the configuration.

--- CodeNotifier.py
+++ CodeNotifier.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-#from oauth import oauth
-from CodeNotifier_config import *
 from oauthtwitter import OAuthApi
-import pprint
+SVNLOOK = "/usr/bin/svnlook"
+FLATHEAD = "flathead"
+
+from CodeNotifier_config import *
+def readConfig():
+    pass
 
 class WebError(Exception):
     def __init__(self, message, code=None):
@@ -31,7 +34,7 @@ def getTags(links):
     links = [link.longurl for link in links]
     tags = []
     for link in links:
-        if "flathead" in link:
+        if FLATHEAD in link:
             TRAC = r'/trac'
             start = link.index(TRAC)+ len(TRAC) + 1
             stop = link.index(r'/', start)
@@ -74,16 +77,24 @@ class BitlyUrl:
     def __len__(self):
         return len(self.shorturl)
 
+APP_KEY=r'pfAHwsfJxkyzR25oLw13VQ'
+APP_SECRET=r'ANihRKuKtubyhH4PZsZIHOVNNoxWomKJeSuOAdodH8c'
+
 class StatusMsg:
-    def __init__(self, status, app_key=r'pfAHwsfJxkyzR25oLw13VQ',
-                 app_secret=r'ANihRKuKtubyhH4PZsZIHOVNNoxWomKJeSuOAdodH8c',
+    def __init__(self, status=None,
+                 app_key=APP_KEY, app_secret=APP_SECRET,
                  user_key=TWIT_TOKEN, user_secret=TWIT_SECRET):
         self.__twitter = OAuthApi(app_key, app_secret, user_key, user_secret)
+        if status is not None:
+            self.setMsg(status)
 
+    def setMsg(self, status):
         self.msg = status
+        if len(self.msg) <= 0:
+            return
         self.__processLinks()
         self.__abbridgeMsg()
-
+        
     def __processLinks(self):
         import re
         self.links = re.findall(r'https?://.+$', self.msg)
@@ -106,7 +117,111 @@ class StatusMsg:
         return self.msg
 
     def send(self):
-        return self.__twitter.UpdateStatus(self.msg)
+        if (self.msg is not None) and (len(self.msg) > 0):
+            return self.__twitter.UpdateStatus(self.msg)
+
+class SvnMsg(StatusMsg):
+    def __init__(self, repos, rev, **kwargs):
+        StatusMsg.__init__(self, **kwargs)
+        self.__getCommit(repos, rev)
+
+    def svnlook(self, indicator, changeset, command):
+        cmd = "%s %s -r %d %s" % (SVNLOOK, command, int(changeset), 
+                                    indicator)
+        import subprocess as sub
+        proc = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE, shell=True)
+        return_code = proc.wait()
+        (stdout, stderr) = proc.communicate()
+        return stdout.strip() 
+
+    def __getCommit(self, repos, rev):
+        import os
+        repos_fs = os.path.join(SVN_FS_ROOT, repos)
+
+        author = self.svnlook(repos_fs, rev, "author")
+        log = self.svnlook(repos_fs, rev, "log")
+        url = SVN_TRAC_FORMAT % (repos, int(rev))
+
+        if self.__refsTicket(log):
+            self.setMsg("")
+        else:
+            self.setMsg(" ".join((author, log, url)))
+
+    def __refsTicket(self, log):
+        import re
+        tickets = re.findall(r'#\d+', log)
+        return len(tickets) > 0
+
+class TracMsg(StatusMsg):
+    def __init__(self, email_msg):
+        text = email_msg.as_string()
+        author = self.__getAuthor(text)
+        url = self.__getUrl(text)
+        log = self.__getLog(text)
+        if len(url) <= 0 or FLATHEAD in url:
+            proj = self.__getProj(text)
+        else:
+            proj = ''
+        self.setMsg(' '.join((author, log, url, proj)))
+
+    def __getAuthor(self, text):
+        import re
+
+        # first look for a changeset
+        answer = re.findall(r'Changes\s*\(by\s*(.+)\)', text)
+        if len(answer) > 0:
+            return answer[0].strip()
+
+        # then look for a comment
+        answer = re.findall(r'Comment\s*\(by\s*(.+)\)', text)
+        if len(answer) > 0:
+            return answer[0].strip()
+
+        # now the person that owns it
+        answer = re.findall(r'\s+Owner:\s*(.+)', text)
+        if len(answer) > 0:
+            return answer[0].strip()
+        
+        return ""
+
+    def __getUrl(self, text):
+        import re
+        answer = re.findall(r'Ticket URL:\s+<(.+)>', text)
+        if len(answer) <= 0:
+            return ""
+        link = answer[0]
+
+        # drop the link to the comment
+        if '#' in link:
+            stop = link.index('#')
+            link = link[:stop]
+
+        # format the link
+        if link.startswith("http"):
+            return link
+        else:
+            return ""
+
+    def __getLog(self, text):
+        import re
+        oneline = re.sub(r'\s+', ' ', text)
+
+        answer = re.findall(r'Comment.*\(.+\):\s+(.+)--', oneline)
+        if len(answer) > 0:
+            return self.__trimLog(answer[0])
+
+        answer = re.findall(r'Comment:\s+(.+)--', oneline)
+        if len(answer) > 0:
+            return self.__trimLog(answer[0])
+
+    def __trimLog(self, text):
+        return text.strip()
+
+    def __getProj(self, text):
+        import re
+        answer = re.findall(r'(.+\s+)<.*>', text)
+        project = answer[-1].strip()
+        return '#' + project.replace(' ', '')
 
 """
 twitter = OAuthApi(consumer_key, consumer_secret)
@@ -125,15 +240,22 @@ print("oauth_token: " + access_token['oauth_token'])
 print("oauth_token_secret: " + access_token['oauth_token_secret'])
 """
 
+def getToken():
+    pass
+
 if __name__ == "__main__":
-    status = "This is another test "
-    tracurl = "https://flathead.ornl.gov/trac/TranslationService/ticket/1026"
-    svnurl = "https://flathead.ornl.gov/trac/TranslationService/changeset/5050/"
+    readConfig()
+    print dir()
 
-    msg = StatusMsg(status + tracurl)
-    print "TRAC:", msg
-    msg.send()
-
-    msg = StatusMsg(status + svnurl)
-    print "SVN:", msg
-    #msg.send()
+    import sys
+    if sys.argv[1] == "svn":
+        (repos, rev) = sys.argv[2:]
+        msg = SvnMsg(repos, rev)
+    elif sys.argv[1] == "trac":
+        import email
+        email_msg = email.message_from_file(sys.stdin)
+        msg = TracMsg(email_msg)
+    else:
+        print "need to specify either 'svn' or 'trac' as mode"
+        sys.exit(-1)
+    print msg
